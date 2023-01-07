4c67634abd76cb7c45f2a8573a3aa14eb6b9a210
Jailene <jaileneveras8@gmail.com>
2023-01-07 02:22:08 -0500

    used app.get to display the object in db.json

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const path = require('path')
 const app = express(); 
 const PORT= 3001;
 const fs = require("fs")
+const notes = require("./Develop/db/db.json")
 
-app.get("/notes",(req,res) => {
+app.get("/note",(req,res) => {
 res.sendFile(path.join(__dirname,'Develop/public/notes.html'))
 });
 
@@ -12,6 +13,9 @@ app.get("/*",(req,res) => {
     res.sendFile(path.join(__dirname,'Develop/public/index.html'))
     });
 
+    app.get("/api/notes",(req,res)=>{
+        res.json(notes);
+    })
 
 app.listen(PORT,()=>{
 console.log(`server running on port ${PORT}`);
