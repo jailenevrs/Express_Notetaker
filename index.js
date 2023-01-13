723b796f23972abc035ddeefc64be561858806c5
Jailene <jaileneveras8@gmail.com>
2023-01-13 12:42:12 -0500

    updated line 5 on index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const apiRoutes = require("./routes/apiRoutes")
 const htmlRoutes =require("./routes/htmlRoutes")
 const app = express(); 
-const PORT= 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
